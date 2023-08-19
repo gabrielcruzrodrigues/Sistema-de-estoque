9026fdb82ba971c2d423d09d4f6b0308d703bbc9
unknown <contatogabrielcruzrodrigues@gmail.com>
2023-08-19 01:07:36 -0300

    created updated function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
-import org.example.Product;
 
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.Locale;
 
 public class Main {
     static Product[] products = new Product[10];
@@ -25,6 +24,7 @@ public class Main {
         System.out.println("1.listar todos os produtos");
         System.out.println("2.adicionar unidades a um produto já cadastrado");
         System.out.println("3.remover unidades de um produto já cadastrado");
+        System.out.println("4.atualizar dados");
         System.out.println("====================================================");
         System.out.print("Digite a opção desejada: ");
         int res = scan.nextInt();
@@ -36,6 +36,8 @@ public class Main {
             addProduct();
         } else if (res == 3) {
             removeProduct();
+        } else if (res == 4){
+            updateProduct();
         } else {
             System.out.println("Selecione uma opção valida");
             menu();
@@ -67,7 +69,6 @@ public class Main {
         int quantity = scan.nextInt();
 
         int indice = 0;
-        boolean register = false;
 
         for (Product i : products) {
             if (i == null) {
@@ -107,6 +108,69 @@ public class Main {
             System.out.println("item removido com sucesso.");
         } else {
             System.out.println("Item não encontrado no estoque.");
+        }
+
+        menu();
+        scan.close();
+    }
+
+    public static void updateProduct() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o nome do produto que quer atualizar: ");
+        String produto = scan.nextLine();
+        System.out.println("----------------------------------------------------");
+
+        boolean confirm = false;
+
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] != null && products[i].getName().equalsIgnoreCase(produto)) {
+                String name = "";
+                double price = 0.0;
+                int quantity = 0;
+
+                System.out.println("Quer atualizar o nome? (s/n): ");
+                String OptionName = scan.next();
+
+                if (Objects.equals(OptionName, "s")) {
+                    System.out.print("Digite o nome do produto: ");
+                    scan.nextLine();
+                    name = scan.nextLine();
+
+                    products[i].setName(name);
+                }
+
+                System.out.println("----------------------------------------------------");
+                System.out.println("Quer atualizar o preço? (s/n): ");
+                String OptionPrice = scan.next();
+
+                if(Objects.equals(OptionPrice, "s")) {
+                    System.out.print("Digite o preço do produto: ");
+                    price = scan.nextDouble();
+
+                    products[i].setPrice(price);
+                }
+
+                System.out.println("----------------------------------------------------");
+                System.out.println("Quer atualizar a quantidade? (s/n): ");
+                String OptionQuantity = scan.next();
+
+                if (Objects.equals(OptionQuantity, "s")) {
+                    System.out.print("Digite a quantidade de produtos para estoque: ");
+                    quantity = scan.nextInt();
+
+                    products[i].setQuantity(quantity);
+                }
+
+                System.out.println("----------------------------------------------------");
+                confirm = true;
+            }
+        }
+
+        if (confirm) {
+            System.out.println("Produto atualizado com sucesso!");
+        } else {
+            System.out.println("Nenhum produto foi encontrado com esse nome");
         }
 
         menu();
